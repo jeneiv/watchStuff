7aca47718acf04c1031d507c486163e9b2f43d34
Viktor Jenei <>
2018-04-04 16:07:10 +0200

    - Addedd Launch Interface Controller to Watch Extension - Changed date handling in Launch Model - Added Complication Data Handling Logic - Added Complication Time Travel related methods -

---
+++ WatchAppWatch Extension/Controller/LaunchInterfaceController.swift
@@ -0,0 +1,9 @@
+//
+//  LaunchInterfaceController.swift
+//  WatchAppWatch Extension
+//
+//  Created by Viktor Jenei on 3/29/18.
+//  Copyright © 2018 Viktor Jenei. All rights reserved.
+//
+
+import Foundation
